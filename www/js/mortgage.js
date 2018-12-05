9b70d3890211255d4d9f8ab82e1f429a9130c818
JapaneseFireBmb <markrmayeda@gmail.com>
2018-12-05 08:51:06 -0800

    Started on table

--- www/js/mortgage.js
+++ www/js/mortgage.js
@@ -3,7 +3,7 @@ function select() {
 	xhttp.onreadystatechange = function() {
     	if (this.readyState == 4 && this.status == 200) {
        		var parse = JSON.parse(xhttp.responseText);
-       		var state = "<select id=\"stateInfo\" style=\"border: outset;\" onchange=\"stateInsurance()\"><option value = \"\">- Select -</option>"
+       		var state = "<select id=\"stateInfo\" style=\"border: outset;\" onchange=\"stateInsurance(); makeTable();\"><option value = \"\">- Select -</option>"
        		for(var i = 0; i < parse.length; i++) {
        			state += "<option value= \"" + parse[i].rate + "\">" + parse[i].state + "</option>";
        		}
@@ -35,4 +35,14 @@ function DPAmntChange() {
     var pcnt = document.getElementById('DPPcnt').value * .001;
     document.getElementById('DPAmnt').value = principle * pcnt;
   }
+}
+
+function makeTable() {
+  var table = "<table><tr><th>Period</th><th>Beginning Principle</th><th>Monthly Payment</th><th>Payment on Interest</th><Payment on Principle</th><th>Remaining Principle</th></tr>"
+  var principle = parseFloat(document.getElementById('principle').value);
+  var interest = parseFloat(document.getElementById('interest').value) / 100;
+  var downPayment = parseFloat(document.getElementById('DPAmnt').value);
+  var lenght = parseInt(document.getElementById('length').value);
+
+  document.getElementById('table').innerHTML = table;
 }
